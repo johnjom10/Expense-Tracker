4df9105f213e1d44f68bb7eb756352d31ac949e7
hari751995 <hari751995@gmail.com>
2016-05-30 11:49:32 +0530

    Pie With Legend

--- monthly_report/pie.js
+++ monthly_report/pie.js
@@ -8,61 +8,110 @@ var htmlStub = ''
 
 var pie = (function () {
   return {
-    pieChart: function (categories, percentage, callback) {
+    pieChart: function (data, callback) {
       jsdom.env({
         features: { QuerySelector: true }, html: htmlStub, done: function (errors, window) {
           var html = window.document.querySelector('html')
 
-          var w = 250 // width
-          var h = 250 // height
-          var r = 120 // radius
+          var SEGMENT = 'category'
+          var DATA = 'percentage'
 
-          var data = []
-          var color = d3.scale.category20c() // builtin range of colors
-          for (var i = 0; i < categories.length; i++) {
-            data[i] = {}
-            data[i].label = categories[i]
-            data[i].value = percentage[i]
-          }
-          // generate the dataviz
+          var width = 700
+          var height = 450
+          var radius = Math.min(width, height) / 3
 
-          var vis = d3.select(html)
-            .append('svg:svg') // create the SVG element inside the <body>
-            .data([data]) // associate our data with the document
-            .attr('width', w) // set the width and height of our visualization (these will be attributes of the <svg> tag
-            .attr('height', h)
-            .attr('margin', 'auto')
+          var COLOR_10 = '#25628e'
+          var COLOR_9 = '#2f7eb6'
+          var COLOR_8 = '#348ccb'
+          var COLOR_7 = '#4998d0'
+          var COLOR_6 = '#5da3d5'
+          var COLOR_5 = '#71afda'
+          var COLOR_4 = '#85bae0'
+          var COLOR_3 = '#9ac6e5'
+          var COLOR_2 = '#aed1ea'
+          var COLOR_1 = '#c2ddef'
+
+          var color = d3.scale.ordinal()
+            .range([COLOR_1, COLOR_2, COLOR_3,
+              COLOR_4, COLOR_5, COLOR_6, COLOR_7, COLOR_8, COLOR_9, COLOR_10])
+
+          var arc = d3.svg.arc()
+            .outerRadius(radius - 10)
+            .innerRadius(0)
+
+          var pie = d3.layout.pie()
+            .sort(null)
+            .value(function (d) {
+              return d[DATA]
+            })
+
+          var svg = d3.select(html).append('svg')
+            .attr('width', width)
+            .attr('height', height)
             .attr('version', '1.1')
             .attr('xmlns', 'http://www.w3.org/2000/svg')
-            .append('svg:g') // make a group to hold our pie chart
-            .attr('transform', 'translate(' + r + ',' + r + ')')
-          var arc = d3.svg.arc() // this will create <path> elements for us using arc data
-            .outerRadius(r)
-          var pie = d3.layout.pie() // this will create arc data for us given a list of values
-            .value(function (d) { return d.value }) // we must tell it out to access the value of each element in our data array
-          var arcs = vis.selectAll('g.slice') // this selects all <g> elements with class slice (there aren't any yet)
-            .data(pie) // associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
-            .enter() // this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
-            .append('svg:g') // create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
-            .attr('class', 'slice') // allow us to style things in the slices (like text)
-          arcs.append('svg:path')
-            .attr('fill', function (d, i) { return color(i) }) // set the color for each slice to be chosen from the color function defined above
-            .attr('d', arc) // this creates the actual SVG path using the associated data (pie) with the arc drawing function
-          arcs.append('svg:text') // add a label to each slice
-            .attr('transform', function (d) { // set the label's origin to the center of the arc
-              // we have to make sure to set these before calling arc.centroid
-              d.innerRadius = 0
-              d.outerRadius = r
-              return 'translate(' + arc.centroid(d) + ')' // this gives us a pair of coordinates like [50, 50]
-            })
-            .attr('text-anchor', 'middle') // center the text on it's origin
-            .text(function (d, i) { return data[i].label }) // make the client-side script manipulate the circle at client side)
+            .append('g')
+            .attr('transform', 'translate(' + width / 3 + ',' + height / 2 + ')')
 
+          var drawD3Document = function (data) {
+            data.forEach(function (d) {
+              d[DATA] = +d[DATA]
+            })
+            var g = svg.selectAll('.arc')
+              .data(pie(data))
+              .enter().append('g')
+              .attr('class', 'arc')
+
+            var count = 0
+
+            g.append('path')
+              .attr('d', arc)
+              .attr('id', function (d) { return 'arc-' + (count++) })
+              .style('fill', function (d) {
+                return color(d.data[SEGMENT])
+              })
+            g.append('text').attr('transform', function (d) {
+              return 'translate(' + arc.centroid(d) + ')'
+            }).attr('dy', '.35em').style('text-anchor', 'middle')
+              .text(function (d) {
+                return (d.data[DATA] + '%')
+              })
+
+            count = 0
+            var legend = svg.selectAll('.legend')
+              .data(data).enter()
+              .append('g').attr('class', 'legend')
+              .attr('legend-id', function (d) {
+                return count++
+              })
+              .attr('transform', function (d, i) {
+                return 'translate(-60,' + (-70 + i * 20) + ')'
+              })
+              .on('click', function () {
+                var arc = d3.select('#arc-' + (this).attr('legend-id'))
+                arc.style('opacity', 0.3)
+                setTimeout(function () {
+                  arc.style('opacity', 1)
+                }, 1000)
+              })
+
+            legend.append('rect')
+              .attr('x', width / 2)
+              .attr('width', 18).attr('height', 18)
+              .style('fill', function (d) {
+                return color(d[SEGMENT])
+              })
+            legend.append('text').attr('x', width / 2)
+              .attr('y', 9).attr('dy', '.35em')
+              .style('text-anchor', 'end').text(function (d) {
+              return (d[SEGMENT])
+            })
+          }
+          drawD3Document(data)
           // save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
           var svgsrc = window.document.documentElement.innerHTML
-          var svg = strip(svgsrc, '<svg><path><text><g>')
-
-          callback(svg)
+          var svgStripped = strip(svgsrc, '<svg><path><text><g><rect>')
+          callback(svgStripped)
         } // end jsDom done callback
       })
     }
